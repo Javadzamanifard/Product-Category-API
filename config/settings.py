2f52ef3ec58cd3a5390504e626f195d21a95b8fc
javadzamanifard <javadzamanii.1374@gmail.com>
2025-10-08 08:07:39 -0700

    Implement global pagination settings

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framwork.pagination.PageNumberPageination',
+    'PAGE_SIZE': 10,
+}
