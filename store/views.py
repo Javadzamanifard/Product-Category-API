5ce93068f93407785c31803986795b7420cf56fe
javadzamanifard <javadzamanii.1374@gmail.com>
2025-10-08 08:04:44 -0700

    Implement custom pagination

--- store/views.py
+++ store/views.py
@@ -4,12 +4,16 @@ from serializers import CategorySerializer, ProductSerializer
 
 from models import Category, Product
 
+from .pagination import DefaultPagination
+
 
 class CategoryApiView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = DefaultPagination
 
 
 class ProductApiView(ModelViewSet):
     queryset = Product.objects.select_related('category').all()
     serializer_class = ProductSerializer
+    pagination_class = DefaultPagination
